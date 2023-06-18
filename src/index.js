8bc7341370ddb00f08d3b69872da584bd5d4444e
Sandra G <sandyjtech@gmail.com>
2023-06-18 07:16:58 -0700

    created movie form with fetch()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
+   
+    const inputForm = document.querySelector("form");
   
-}
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const input = document.querySelector("input#searchByID");
+        console.log(input.value);
 
-document.addEventListener('DOMContentLoaded', init);
+        //Fetch Method wait for movie id to be returned and fetch
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            //Create Movie object
+          const title =  document.querySelector("#movieDetails h4");
+          const description =  document.querySelector("#movieDetails p");
+//Displays Movie on fetch Id
+          title.innerText = data.title;
+          p.innerText = data.description;
+        });
+      }); 
+
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
